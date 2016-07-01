6a822f9fa117e948f1edffa28e241176be8060f3
immccull <imccullough@bren.ucsb.edu>
2016-07-01 15:52:27 -0700

    code improved, but not done

--- 11-advanced-R/scripts/zillow_data_wrangling.R
+++ 11-advanced-R/scripts/zillow_data_wrangling.R
@@ -4,7 +4,8 @@ library(readr)
 library(tidyr)
 library(dplyr)
 
-setwd("/Users/brun/Data/Zillow")
+#setwd("/Users/brun/Data/Zillow")
+setwd('/Users/mccullough/Documents/postdoc-training/postdoc-training/11-advanced-R/data/Zillow')
 
 # read the data ----
 # Listing Price
@@ -22,7 +23,7 @@ ls()
 
 # Listed price ----
 
-# To convert this messy data into tidy data
+# To convert these messy data into tidy data
 # we need two verbs. First we need to gather
 # together all the columns that aren't variables
 price_listing2 <- price_listing_median %>%
@@ -36,11 +37,23 @@ price_listing3 <- price_listing2 %>%
 price_listing3
 
 # Sale Price ----
-price_sale2 <- price_sale_median %>% 
-  select(-(8:172)) %>%
+cols <- c(1:7, 173:249) # correspond to all desired columns
+price_sale_truncate <- price_sale_median[,cols]
+
+# gather all columns that aren't variables
+price_sale2 <- price_sale_truncate %>% 
   gather(date, price_sale_med, 8:84, na.rm = TRUE)
 price_sale2
 
+# Then separate the date variable into
+# year and month
 price_sale3 <- price_sale2 %>%
   separate(date, c("year", "month"), "-")
 price_sale3
+
+#### join list price and sale price tables ####
+#summarise(price_listing3)
+
+# concatentate year and month column to create yearmonth column on which to perform join
+price_sale3$yearmonth <- 
+price_listing3$yearmonth <- 
